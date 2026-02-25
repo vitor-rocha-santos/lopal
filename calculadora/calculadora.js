58711f3482f640e0f0b80243d01abde8adc2c41b
vitor-rocha-santos <vitortri2009@gmail.com>
2026-02-25 16:46:02 -0300

    calculadora

---
+++ calculadora/calculadora.js
@@ -0,0 +1,25 @@
+
+    let resultado = 0.0;
+    let operando1;
+    let operando2;
+    let operador ;
+
+    operando1 = parseFloat ( prompt("Digite o primeiro número: "));
+    operando2 = parseFloat (prompt ( "Digite o segundo número: "));
+    operador = prompt("Digite uma das operações (+ - * / ): ")
+
+    if ( operador === "+ "){
+            resultado = operando1 + operando2;
+} else if (operador === "-" ) {
+    resultado = operando1 - operando2;
+} else if (operador === "*" ){
+    resultado = operando1 *operando2;
+} else if (operador === "/" ){
+    resultado = operando1 / operando2;
+} else {
+    alert("Operador inválido!");
+    return
+}
+
+
+alert"Resultado: " +

fd2691b6ddfa6d850a8e3dcba5a0bcab523f2c0d
vitor-rocha-santos <vitortri2009@gmail.com>
2026-03-11 16:46:30 -0300

    feat: Calculadora ter minada e iniciada Urna EletrÔnica

---
+++ Idade/idade.js
@@ -0,0 +1,10 @@
+function idade(){
+ let idade; 
+    do{
+        idade = parseInt(prompt("Informe sua idade (valore aceitos de 5 a 150:" ));
+    } while (idade < 5 || idade > 150 ||
+
+
+
+}
+   
---
+++ urna/urna.js
@@ -0,0 +1,8 @@
+function urna(){
+    let candidato_a = 0;
+    let candidato_b = 0;
+    let brancos = 0;
+    let nulos = 0;
+    let total = 0;
+    
+}
